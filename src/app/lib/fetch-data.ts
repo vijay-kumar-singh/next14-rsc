7707613ec27a42c59b448b4d3d0506c7ca202c26
Vijay Singh <vijaysingh.nl@gmail.com>
2024-01-03 21:34:57 +0100

    get Query param

--- src/app/lib/fetch-data.ts
+++ src/app/lib/fetch-data.ts
@@ -1,11 +1,14 @@
 import { cache } from 'react'
 import 'server-only';
 import { headers } from 'next/headers';
+import GET from './get-url-query'
 
 const fetchData = cache(async (id: string) => {
     const headersList = headers();
     const baseUrl = headersList.get('host');
-    console.log(baseUrl);
+    const req = { url: 'http://example.com/?queryParamName=queryParamValue' };
+    const result = await GET(req);
+   console.log(result)
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${id}`,
     {
--- src/app/lib/get-url-query.ts
+++ src/app/lib/get-url-query.ts
@@ -1,6 +1,7 @@
-export async function GET(req:any) {
+export default async function GET(req:any) {
     const url = new URL(req.url);
     const searchParams = url.searchParams;
     const queryParam = searchParams.get('queryParamName');
     // Use the queryParam value as needed
+    return queryParam;
   }
